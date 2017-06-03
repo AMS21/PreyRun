8d4237d46248606a6b68cacacc5beffbe83f2bc0
CppAndre <hansfritzenmeister@yahoo.de>
2017-06-03 23:35:28 +0200

    Tabs and spaces

--- src/PreyRun/Logging.hpp
+++ src/PreyRun/Logging.hpp
@@ -5,34 +5,34 @@
 
 namespace pr
 {
-	/* Prints: Console, Logfile
-		   Format: 'PreyRun: %s\n'
+	/*	Prints: Console, Logfile
+		Format: 'PreyRun: %s\n'
 		*/
 	void Log(const char *fmt, ...);
 
-	/* Prints: Console, Logfile
-		   Debug : Will only print if the 'PR_DEBUG' macro is set
-		   Format: 'PreyRun DBG: %s\n'
+	/*	Prints: Console, Logfile
+		Debug : Will only print if the 'PR_DEBUG' macro is set
+		Format: 'PreyRun DBG: %s\n'
 		*/
 	void DebugLog(const char *fmt, ...);
 
-	/* Prints: Console, Logfile
-		   Format: '__FUNCTION__: %s\n'
+	/*	Prints: Console, Logfile
+		Format: '__FUNCTION__: %s\n'
 		*/
 	void FunctionLog(const char *func, const char *fmt, ...);
 
-	/* Prints: Console
-		   Format: '%s\n'
+	/*	Prints: Console
+		Format: '%s\n'
 		*/
 	void ConsoleWrite(const char *fmt, ...);
 
-	/* Prints: Console, Logfile
-		   Format: 'PreyRun: %s\n'
+	/*	Prints: Console, Logfile
+		Format: 'PreyRun: %s\n'
 		*/
 	void Warning(const char *fmt, ...);
 
-	/* Prints: Console, Logfile
-		   Format: 'PreyRun: %s\n'
+	/*	Prints: Console, Logfile
+		Format: 'PreyRun: %s\n'
 		*/
 	void Error(const char *fmt, ...);
 }
